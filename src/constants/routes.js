563f45f5a87dd8fe2d027ec9a40440e092617b72
Reinhardt Rijna <reinhardtrijna@gmail.com>
2020-12-19 11:35:59 +0100

    v1

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,4 @@
-export const HOME = "/";
-export const BROWSE = "/browse";
-export const SIGN_UP = "/sign-up";
-export const SIGN_IN = "/sign-in";
+export const HOME = "/netflix-recreation";
+export const BROWSE = "/netflix-recreation/browse";
+export const SIGN_UP = "/netflix-recreation/sign-up";
+export const SIGN_IN = "/netflix-recreation/sign-in";
